e7ec863e3cdde036b539a950fc5057d095cc7a1e
JGulbronson <jgulbronson@gmail.com>
2014-09-27 15:55:24 -0400

    Basic controls working, needs persistence

--- src/golf-app.c
+++ src/golf-app.c
@@ -1,47 +1,64 @@
 #include <pebble.h>
 
 /* TODO
- * Display current hole
- * Display current number of strokes
- * Make back decrease the current hole
- * Make select increase the current hole
- * Make up increase the number of strokes
- * Make down decrease the number of strokes
+ * Some persistence of some sort
  * Interface with phone (Android app)
  */
 
 static Window *window;
 static TextLayer *current_hole_text_layer;
-static char *hole_label = "Hole ";
+static TextLayer *current_stroke_text_layer;
+static char hole_label[6] = "Hole ";
 static int current_hole = 1;
 static char current_hole_string[2];
 static char stroke_label[8] = "Stroke ";
 static int strokes = 0;
 static char current_strokes_string[2];
 
+static void update_current_hole_label() {
+  static char label[10];
+  strncpy(label, hole_label, sizeof(hole_label));
+  char current_hole_label[3];
+  snprintf(current_hole_label, sizeof(current_hole_label), "%d", current_hole);
+  strncat(label, current_hole_label, sizeof(current_hole_label));
+  text_layer_set_text(current_hole_text_layer, label);
+}
+
+static void update_current_stroke_label() {
+  static char label[10];
+  strncpy(label, stroke_label, sizeof(stroke_label));
+  char current_stroke_label[3];
+  snprintf(current_stroke_label, sizeof(current_stroke_label), "%d", strokes);
+  strncat(label, current_stroke_label, sizeof(current_stroke_label));
+  text_layer_set_text(current_stroke_text_layer, label);
+}
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(current_hole_text_layer, "Select");
+  if (current_hole < 18) {
+    current_hole += 1;
+  }
+  update_current_hole_label();
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   if (strokes <= 9) {
     strokes += 1;
   }
-  static char label[10];
-  strncpy(label, stroke_label, sizeof(stroke_label));
-  char current_stroke_label[10];
-  snprintf(current_stroke_label, sizeof(current_stroke_label), "%d", strokes);
-  strncat(label, current_stroke_label, sizeof(current_stroke_label));
-  text_layer_set_text(current_hole_text_layer, label);
+  update_current_stroke_label();
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(current_hole_text_layer, "Down");
+  if (strokes >= 1) {
+    strokes -= 1;
+  }
+  update_current_stroke_label();
 }
 
 static void back_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(current_hole_text_layer, "Back");
+  if (current_hole > 1) {
+    current_hole -= 1;
+  }
+  update_current_hole_label();
 }
 
 static void click_config_provider(void *context) {
@@ -59,11 +76,22 @@ static void window_load(Window *window) {
     .origin = { 0, 30 },
     .size = { bounds.size.w, 30 }
   });
-  text_layer_set_text(current_hole_text_layer, "Press a button");
   text_layer_set_font(current_hole_text_layer, fonts_get_system_font(
         FONT_KEY_GOTHIC_28));
   text_layer_set_text_alignment(current_hole_text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(current_hole_text_layer));
+
+  current_stroke_text_layer = text_layer_create((GRect) {
+    .origin = { 0, 80 },
+    .size = { bounds.size.w, 30 }
+  });
+  text_layer_set_font(current_stroke_text_layer, fonts_get_system_font(
+        FONT_KEY_GOTHIC_28));
+  text_layer_set_text_alignment(current_stroke_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(current_stroke_text_layer));
+
+  text_layer_set_text(current_stroke_text_layer, "Stroke 1");
+  text_layer_set_text(current_hole_text_layer, "Hole 1");
 }
 
 static void window_unload(Window *window) {
